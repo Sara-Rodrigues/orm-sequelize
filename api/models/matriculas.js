2c8707bc86f1ffc82447741a9f78ae9633840031
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-04-26 10:02:25 -0300

    referenciando tabelas

--- api/models/matriculas.js
+++ api/models/matriculas.js
@@ -3,8 +3,12 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class Matriculas extends Model {
         static associate(models) {
-            Matriculas.belongsTo(models.Pessoas)
-            Matriculas.belongsTo(models.Turmas)
+            Matriculas.belongsTo(models.Pessoas, {
+                foreignKey: 'estudante_id'
+            })
+            Matriculas.belongsTo(models.Turmas, {
+                foreignKey: 'turma_id'
+            })
         }
     };
     Matriculas.init({
