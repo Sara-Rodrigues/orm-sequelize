800471bace7e617923b8183472ee8fd50bb7bc38
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-04-26 09:36:42 -0300

    fazendo associações

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -3,7 +3,12 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class Pessoas extends Model {
         static associate(models) {
-
+            Pessoas.hasMany(models.Turmas, {
+                foreignKey: 'docente_id'
+            })
+            Pessoas.hasMany(models.Matriculas, {
+                foreignKey: 'estudante_id'
+            })
         }
     };
     Pessoas.init({
