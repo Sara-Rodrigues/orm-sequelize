344b9ff37d199ba8c3eefe86274b674e80adcf13
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-04-27 15:20:39 -0300

    atualizando matricula

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -80,6 +80,25 @@ class PessoaController {
         }
     }
 
+    static async atualizaMatricula(req, res) {
+        const { estudanteId, matriculaId } = req.params
+        const novasInfos = req.body
+        try {
+            await database.Matriculas.update(novasInfos, {
+                where: {
+                    id: Number(matriculaId),
+                    estudante_id: Number(estudanteId)
+                }
+            })
+            const matriculaAtualizada = await database.Matriculas.findOne({
+                where: { id: Number(matriculaId) }
+            })
+            return res.status(200).json(matriculaAtualizada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
 }
 
 module.exports = PessoaController
