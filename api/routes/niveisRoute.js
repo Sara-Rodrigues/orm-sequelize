31395fa0b6c96f9f942a3be2281ee948f5c372dd
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-04-27 18:02:56 -0300

    restaurando registros

--- api/routes/niveisRoute.js
+++ api/routes/niveisRoute.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 const NivelController = require('../controllers/NivelController')
+const { route } = require('./pessoasRoute')
 
 const router = Router()
 
 router.get('/niveis', NivelController.pegaTodosOsNiveis)
 router.get('/niveis/:id', NivelController.pegaUmNivel)
 router.post('/niveis', NivelController.criaNivel)
+router.post('/niveis/:id/restaura', NivelController.restauraNivel)
 router.put('/niveis/:id', NivelController.atualizaNivel)
 router.delete('/niveis/:id', NivelController.apagaNivel)
 
