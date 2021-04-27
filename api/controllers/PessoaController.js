b7bd4940937dea99d4e032031195d87aea3c4585
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-04-27 16:01:33 -0300

    deletando uma matricula

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -99,6 +99,16 @@ class PessoaController {
         }
     }
 
+    static async apagaMatricula(req, res) {
+        const { estudanteId, matriculaId } = req.params
+        try {
+            await database.Matriculas.destroy({ where: { id: Number(matriculaId) } })
+            return res.status(200).json({ mensagem: `id ${matriculaId} deletado` })
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
 }
 
 module.exports = PessoaController
