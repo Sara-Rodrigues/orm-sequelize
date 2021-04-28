cd89e50040b676df428474c8d4b5bb93d2082a00
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-04-28 11:25:20 -0300

    escopo  modelo - trazendo somente pessoas com matriculas ativas

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -19,6 +19,7 @@ module.exports = (sequelize, DataTypes) => {
     }, {
         sequelize,
         paranoid: true,
+        defaultScope: { where: { ativo: true } },
         modelName: 'Pessoas',
     });
     return Pessoas;
