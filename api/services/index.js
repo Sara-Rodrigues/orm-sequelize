fdd171c5fd682f950757c0d613354e16f9d7c068
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-04-30 10:59:28 -0300

    refatorando serviços

--- api/services/index.js
+++ api/services/index.js
@@ -1,6 +1,7 @@
 const PessoasServices = require('./PessoasServices')
 const TurmasServices = require('./TurmasServices')
 const NiveisServices = require('./NiveisServices')
+const MatriculasServices = require('./MatriculasServices')
 
 // este arquivo é o ponto de entrada (entrypoint) chamado index
 // que só chama os arquivos correspondente 
@@ -8,5 +9,6 @@ const NiveisServices = require('./NiveisServices')
 module.exports = {
     PessoasServices: PessoasServices,
     TurmasServices: TurmasServices,
-    NiveisServices: NiveisServices
+    NiveisServices: NiveisServices,
+    MatriculasServices: MatriculasServices
 }
